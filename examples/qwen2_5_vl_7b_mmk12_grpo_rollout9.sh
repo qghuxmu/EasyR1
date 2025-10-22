76fd5bbb1f0acc136d0e4cbd521fb6da3b777fc9
qghuxmu <limazhiluyao@gmail.com>
2025-10-21 23:56:49 -0700

    update

---
+++ examples/qwen2_5_vl_7b_mmk12_grpo_rollout9.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct  # replace it with your local file path
+
+python3 -m verl.trainer.main \
+    config=examples/config.yaml \
+    data.train_files=limazhiluyao/MMK12@train \
+    data.val_files=limazhiluyao/MMK12@test \
+    data.prompt_key=question \
+    data.image_key=image \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    worker.rollout.n=9 \
+    trainer.experiment_name=qwen2_5_vl_7b_mmk12_grpo_rollout9 \
+    trainer.n_gpus_per_node=8 \
+    trainer.save_checkpoint_path=/mnt/blob-hptrainingwesteurope-pretraining-out/qingguo/easyr1/qwen2_5_vl_7b_mmk12_grpo_rollout9 \
+    trainer.load_checkpoint_path=/mnt/blob-hptrainingwesteurope-pretraining-out/qingguo/easyr1/qwen2_5_vl_7b_mmk12_grpo_rollout9 \
+    trainer.save_limit=-1
