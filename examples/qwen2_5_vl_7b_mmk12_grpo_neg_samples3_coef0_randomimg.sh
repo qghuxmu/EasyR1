cb6f89067cf9f9d071dace2f71a84368ce9e83fc
qghuxmu <limazhiluyao@gmail.com>
2025-10-23 21:19:33 -0700

    update

--- examples/qwen2_5_vl_7b_mmk12_grpo_neg_samples3_coef0_randomimg.sh
+++ examples/qwen2_5_vl_7b_mmk12_grpo_neg_samples3_coef0_randomimg.sh
@@ -3,6 +3,7 @@
 set -x
 
 MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct  # replace it with your local file path
+export WANDB_API_KEY="4039be8f46d3f1bf00274658db376a4dee152cd4"
 
 python3 -m verl.trainer.main \
     config=examples/config.yaml \
